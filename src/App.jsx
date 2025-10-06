8a46f71b908d0e7a4bdd22e4c7e717244ba7c64f
DSaca95 <dalmadisanyi@gmail.com>
2025-10-06 15:15:52 +0200

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,25 @@ function App() {
     })
   }
   return (
-    <div>
+    <div className='app-container'>
       <h1>QR Code Generator</h1>
+      <div className='input'>
       <label htmlFor='text'>URL or Text:</label>
       <input
         name='text'
+        id='text'
         type='text'
         placeholder='e.g. https://example.com'
         value={url}
         onChange={(e) => setUrl(e.target.value)} />
       <button onClick={GenerateQRCodeSvg}>Generate</button>
+      </div>
       {qrcodeSvg && (
         <div className='svg-container' dangerouslySetInnerHTML={{__html: qrcodeSvg }} />
       )}
       {qrcodeSvg && (
-        <a 
-          href={`data:image/svg+xml;charset=utf-8,${encodeURIComponent(qrcodeSvg)}`}
+        <a href={`data:image/svg+xml;charset=utf-8,${encodeURIComponent(qrcodeSvg)}`}
+            alt={url}
           download="qrcode.svg">
             Download
         </a>
